fb439ae0328bfecf48d6161b255af19904a5532b
Ayoub BERIANE <155060685+ayoubberiane@users.noreply.github.com>
2025-03-21 16:27:46 +0100

    Update lesson1.js
    Thanks

--- lesson1.js
+++ lesson1.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Lesson 2 page loaded.");
+// JavaScript for any future interaction if necessary
+document.addEventListener("DOMContentLoaded", function () {
+    const previousButton = document.querySelector('.button[href="video.html"]');
+    const nextButton = document.querySelector('.button[href="lesson2.html"]');
+
+    // Custom JavaScript logic can be added here for future interactions if needed
 });
