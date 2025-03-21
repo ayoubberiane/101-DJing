907e4ac56d14fb7a9259d66db9a4be839e8b538f
Ayoub BERIANE <155060685+ayoubberiane@users.noreply.github.com>
2025-03-21 19:23:39 +0100

    Create commingsoon3.js
    Thanks

---
+++ commingsoon3.js
@@ -0,0 +1,6 @@
+// JavaScript to redirect after 2 seconds
+function redirectToHomepage() {
+    setTimeout(function() {
+        window.location.href = "index.html"; // Redirect to homepage
+    }, 2000); // 2 seconds delay
+}
