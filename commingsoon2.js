70230d76688fb6e972b28a2af131385ee8e14f58
Ayoub BERIANE <155060685+ayoubberiane@users.noreply.github.com>
2025-03-21 19:14:54 +0100

    Create commingsoon2.js
    thanks

---
+++ commingsoon2.js
@@ -0,0 +1,6 @@
+function redirectToHomepage() {
+    // After 2 seconds, the page will redirect to the homepage
+    setTimeout(function() {
+        window.location.href = 'index.html'; // Redirect after 2 seconds
+    }, 2000); // 2000 milliseconds = 2 seconds
+}
